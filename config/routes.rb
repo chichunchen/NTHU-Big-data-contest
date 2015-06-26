c01c36dc0ea9d0c75e9beed5a64a6089ba2f23dc
chichunchen <chi.chun_chen844@icloud.com>
2015-06-26 17:01:44 +0800

    Fix bug: devise route should not below user resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :users
+  devise_for :users
+  resources :users, only: [:index, :show]
   resources :courses
   root 'welcome#index'
 
@@ -14,7 +15,6 @@ Rails.application.routes.draw do
     resources :users
   end
 
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,22 @@ Role.create([name: 'admin'])
 
 # u = User.create([email: 'admin@gmail.com', password: 'password', password_confirmation: 'password'])
 # u.roles << Role.find(name: 'admin')
+
+
+# 最高權限帳號
+admin = User.create([
+                name: '管理者',
+                email: 'root@gmail.com',
+                password: '12345678',
+                password_confirmation: '12345678',
+                nation: "中華民國（台灣）籍"],
+                identity: "0123456789"],
+                birthday: "Thu, 25 Aug 1988"],
+                gender: "male"],
+                experience: "xxx"],
+                course: "yyy"],
+                phone: "0912345678"],
+                cell_phone: "0912345678"],
+                address: "zzz"]
+                )
+admin.role = Role.find(1)
