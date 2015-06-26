e22efaa66a70ee2f24b4f6f6ff07d033543e9432
chichunchen <chi.chun_chen844@icloud.com>
2015-06-27 00:51:18 +0800

    Generate fake user data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,45 @@
 
 Role.create([name: 'admin'])
 
-# u = User.create([email: 'admin@gmail.com', password: 'password', password_confirmation: 'password'])
-# u.roles << Role.find(name: 'admin')
-
-
 # 最高權限帳號
-admin = User.create([
-                name: '管理者',
-                email: 'root@gmail.com',
-                password: '12345678',
-                password_confirmation: '12345678',
-                nation: "中華民國（台灣）籍"],
-                identity: "0123456789"],
-                birthday: "Thu, 25 Aug 1988"],
-                gender: "male"],
-                experience: "xxx"],
-                course: "yyy"],
-                phone: "0912345678"],
-                cell_phone: "0912345678"],
-                address: "zzz"]
-                )
-admin.role = Role.find(1)
+admin = User.create!(
+  name: "管理員",
+  email: "admin@gmail.com",
+  password: 'password',
+  password_confirmation: 'password',
+  nation: ["中華民國（台灣）籍", "外國籍"].sample,
+  identity: Faker::Number.number(10),
+  birthday: "Thu, 25 Aug 1988",
+  gender: "male",
+  experience: Faker::Lorem.paragraph,
+  course: Faker::Lorem.paragraph,
+  phone: Faker::Number.number(8),
+  cell_phone: Faker::Number.number(10),
+  address: Faker::Address.city,
+  has_job_bank: false,
+  agree_job_bank: [true, false].sample,
+  password_job_bank: Faker::Internet.password
+)
+admin.roles << Role.find_by(name: 'admin')
+
+create_user = for i in 1..20 do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: '12345678',
+    password_confirmation: '12345678',
+    nation: ["中華民國（台灣）籍", "外國籍"].sample,
+    identity: Faker::Number.number(10),
+    birthday: Faker::Date.between(20.years.ago, 30.years.ago),
+    gender: ["male", "female"].sample,
+    experience: Faker::Lorem.paragraph,
+    course: Faker::Lorem.paragraph,
+    phone: Faker::Number.number(8),
+    cell_phone: Faker::Number.number(10),
+    address: Faker::Address.city,
+    has_job_bank: [true, false].sample,
+    agree_job_bank: [true, false].sample,
+    password_job_bank: Faker::Internet.password
+  )
+end
+
